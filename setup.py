6518642a3a34e94f3d6eb9bf9643a3be807a653a
Davide Zuanon <39605171+daddi1987@users.noreply.github.com>
2025-02-12 23:17:00 +0100

    Create a distribution folder and file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='DynamicAxisControl',
     version='0.1.0',    
     description='Linear trajectory generator for robotic axes, with the ability to synchronize up to two speed profiles, to maximize performance. Also perfect for calculating the trajectories of the CoreXY Axes.',
-    url='https://github.com/daddi1987/Dynamic-Axis-Control/tree/main',
+    url='https://github.com/daddi1987/DynamicAxisControl',
     author='Davide Zuanon',
     author_email='d.zuanon87@gmail.com',
     license='unlicense',
@@ -19,14 +19,14 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: Historical Permission Notice and Disclaimer (HPND)',  
+        'License :: OSI Approved :: The Unlicense (Unlicense)',  
         'Operating System :: POSIX :: Linux',        
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.10',
     ],
     entry_points={
         'console_scripts': [
-            'Dynamic-Axis-Control = timmins:DynamicAxis_Hello!!!!',
+            'Dynamic-Axis-Control = DynamicAxisControl:DynamicAxis_Ready.............',
         ]
     }
 )
